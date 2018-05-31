deef53cba576e08f2a52ba547e58b60a71997b3c
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-31 12:23:25 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,11 +1,11 @@
 class AttractionsController < ApplicationController
   def index
     @attractions = Attraction.all
-    @user = User.find(session[:user_id])
+    current_user
   end
 
   def show
     @attraction = Attraction.find(params[:id])
-    @user = User.find([session[:user_id]])
+    current_user
   end
 end
